24dac2ab59be9592cb552f9f4b579f7889afb50a
Robert Regan <robert_regan86@yahoo.com>
2021-06-04 20:42:49 -0400

    comments

--- src/components/List.js
+++ src/components/List.js
@@ -1,14 +1,14 @@
 import Todo from './Todo'
-
+// functional component
 const List = ({ todos }) => {
     return (
         <div>
           {todos.map((todo) => 
-            <Todo key={todo.id} todo={todo} />
+            <Todo key={todo.id} todo={todo} /> 
           )}  
         </div>
     )
 }
-
+// pass todos object as a prop, map through the array and give the todo an id and the item. key is necessary to have a unique spot, when trying to render a list of components will throw error without key.
 export default List
 
