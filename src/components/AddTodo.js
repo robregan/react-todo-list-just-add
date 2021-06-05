ad22611ee2303f687cd0c867125c45a001be60b4
Robert Regan <robert_regan86@yahoo.com>
2021-06-04 20:29:05 -0400

    all of the comments

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,25 +1,25 @@
 import { useState } from 'react'
-
+// setting on add prop via destructuring like onClick but add a new todo
 const AddTodo = ({ onAdd }) => { 
     const [item, setItem] = useState('')
+// creating vars to make use of state. initial state is blank 
+    const onSubmit = (e) => { // handler function 
+        e.preventDefault() // changing the behavior of the event using preventDefault so dont have to reload
+        onAdd({item}) // adds item to list w onAdd function pass in item ... still a little confused about this addTodo function
 
-    const onSubmit = (e) => {
-        e.preventDefault()
-        onAdd({item})
-
-        setItem('')
+        setItem('') // setting item value back to empty 
     }
 
     return (
-        <form onSubmit={onSubmit}>
+        <form onSubmit={onSubmit}> // handler function
             <label>Add Todo:</label>
             <input 
                 type="text" 
                 placeholder="Todo Item"
                 value={item}
-                onChange={ (e) => setItem(e.target.value)}
+                onChange={ (e) => setItem(e.target.value)} // anon function, only fires off onchange 
             /> 
-            <input type="submit" value="Add Todo" />
+            <input type="submit" value="Add Todo" /> // submit button
         </form>
     )
 }
