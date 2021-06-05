be43513583878e9d9401a5ad9ebcb113a3ac12b5
Robert Regan <robert_regan86@yahoo.com>
2021-06-04 20:34:37 -0400

    the comments

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,17 @@
-import React from "react";
-
-const Header = (props) => {
+import React from "react"; //so we can use react
+// making the header component
+const Header = (props) => { 
   return (
     <div>
       <h1 style={headerStyle}>{props.title}</h1>
     </div>
   );
 };
-
+// here is the styleeee
 const headerStyle = {
   fontSize: "40px",
   textDecoration: "underline",
 };
+// this is a functional component
 
 export default Header;
